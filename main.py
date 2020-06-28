6b63ee1ee28f11618ef9ca658236235cd1f75d94
Oliver Burke <oliver@predictx.com>
2020-06-28 17:23:32 +0100

    move game variables into a dict
    update ball to use the game variables from dict
    need to do same for paddle

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ import pygame
 class Ball:
     def __init__(
         self,
-        screen,
+        game_setup,
         x_position=None,
         y_position=None,
         x_velocity=10,
         y_velocity=10,
         radius=10,
-        colour="white",
     ):
-        self.screen = screen
+        self.game_setup = game_setup
+        self.screen = game_setup["screen"]
         if x_position and y_position:
             self.x_position = x_position
             self.y_position = y_position
@@ -24,28 +24,29 @@ class Ball:
                 "Position parameters provided, putting ball in default position"
             )
             self.x_position = (
-                screen.get_width() - radius - 20
-            )  # todo fix hack, this 20 is just to make sure it misses the border
-            self.y_position = int(screen.get_height() / 2)
+                game_setup["SCREEN_WIDTH"] - radius - game_setup["BORDER_WIDTH"]
+            )
+            self.y_position = int(game_setup["SCREEN_HEIGHT"] / 2)
         self.x_velocity = -x_velocity
         self.y_velocity = -y_velocity
         self.radius = radius
-        self.colour = pygame.Color(colour)
-        self.bg_colour = pygame.Color("black")
+        self.colour = pygame.Color(game_setup["FOREGROUND_COLOUR"])
+        self.bg_colour = pygame.Color(game_setup["BACKGROUND_COLOUR"])
+        self.border_and_ball_width = self.game_setup["BORDER_WIDTH"] + self.radius
 
     def get_coordinates(self):
         return self.x_position, self.y_position
 
     def touching_border(self):
-        border_width = 10  # todo: not hardcode this
-        width_and_radius = border_width + self.radius
+
+
         if (
-            self.x_position <= width_and_radius
-        ):  # or self.x_position >= self.screen.get_width()-width_and_radius:
+            self.x_position <= self.border_and_ball_width
+        ):
             self.x_velocity = self.x_velocity * -1
         if (
-            self.y_position <= width_and_radius
-            or self.y_position >= self.screen.get_height() - width_and_radius
+            self.y_position <= self.border_and_ball_width
+            or self.y_position >= self.game_setup["SCREEN_HEIGHT"] - self.border_and_ball_width
         ):
             self.y_velocity = self.y_velocity * -1
 
@@ -70,15 +71,13 @@ class Ball:
         self.show()
 
     def check_paddle(self, paddle):
-        border_width = 10  # todo: not hardcode this
-        width_and_radius = border_width + self.radius
-        if self.x_position < self.screen.get_width() - width_and_radius:
+        if self.x_position < self.screen.get_width() - self.border_and_ball_width:
             logging.debug("ball in play")
             return True, 0
         else:
             logging.info("Ball at end")
             paddle_top, paddle_bottom = paddle.get_position()
-            if self.y_position in range(paddle_top, paddle_bottom+1):
+            if self.y_position in range(paddle_top, paddle_bottom + 1):
                 self.x_velocity = self.x_velocity * -1
                 logging.info("Hit - Add Score")
                 return True, 1
@@ -155,16 +154,27 @@ def main():
         logging.basicConfig(level=logging.INFO)
     pygame.init()
     playing = True
-    score = 0
-    SCREEN_WIDTH = 1200
-    SCREEN_HEIGHT = 600
-    BORDER_WIDTH = 10
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    draw_borders(screen, SCREEN_WIDTH, SCREEN_HEIGHT, BORDER_WIDTH)
-    ball = Ball(screen)
+    game_setup = {
+        "score": 0,
+        "SCREEN_WIDTH": 1200,
+        "SCREEN_HEIGHT": 600,
+        "BORDER_WIDTH": 10,
+        "BACKGROUND_COLOUR": 'Black',
+        "FOREGROUND_COLOUR": 'White'
+    }
+    game_setup["screen"] = pygame.display.set_mode(
+        (game_setup["SCREEN_WIDTH"], game_setup["SCREEN_HEIGHT"])
+    )
+    draw_borders(
+        game_setup["screen"],
+        game_setup["SCREEN_WIDTH"],
+        game_setup["SCREEN_HEIGHT"],
+        game_setup["BORDER_WIDTH"],
+    )
+    ball = Ball(game_setup)
     ball.show()
-    paddle = Paddle(screen)
+    paddle = Paddle(game_setup["screen"])
     paddle.show()
 
     while playing:
@@ -176,8 +186,9 @@ def main():
         ball.update()
         paddle.update()
         playing, score_update = ball.check_paddle(paddle)
-        score += score_update
-        logging.info(f"Score is {score}")
+        game_setup['score'] += score_update
+        logging.info(f"Score is {game_setup['score']}")
+
 
 if __name__ == "__main__":
     main()
